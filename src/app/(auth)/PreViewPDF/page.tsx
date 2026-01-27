3f72dfb3ba12d3c1b5e99e307259e7d8265d727f
OASISAsx <122677372+OASISAsx@users.noreply.github.com>
2026-01-28 01:32:06 +0700

    tes

--- src/app/(auth)/PreViewPDF/page.tsx
+++ src/app/(auth)/PreViewPDF/page.tsx
@@ -350,7 +350,7 @@ export default function EnhancedLoanSchedule() {
 
                   <Grid size={4}>
                     <Typography variant="body2" color="text.secondary">
-                      วันที่ทำสัญญาk:
+                      วันที่ทำสัญญากห:
                     </Typography>
                   </Grid>
                   <Grid size={8}>
