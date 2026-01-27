3ff76f1033229ba18e9806551ab5bef70eb621df
OASISAsx <122677372+OASISAsx@users.noreply.github.com>
2026-01-28 00:54:16 +0700

    test

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -205,6 +205,10 @@ export default function DashboardPage() {
       value: mainStatus?.totalContracts ?? 0,
       icon: CreditCard,
       color: "#bf5af2",
+      // change: calcPercentChange(
+      //   mainStatus?.totalContracts ?? 0,
+      //   mainStatus?.previousTotalContracts ?? 0,
+      // ),
     },
     {
       title: "รออนุมัติ",
