963796c502747b46f5a59b9407ab41717de039c0
OASISAsx <122677372+OASISAsx@users.noreply.github.com>
2026-02-10 03:23:08 +0700

    update Design click

--- app/about/PortfolioItem.tsx
+++ app/about/PortfolioItem.tsx
@@ -4,6 +4,7 @@ import { motion } from "motion/react";
 import Image from "next/image";
 import { Project } from "./data";
 import { Box } from "@mui/material";
+import { AdsClick } from "@mui/icons-material";
 
 export default function PortfolioItem({
   project,
@@ -17,35 +18,64 @@ export default function PortfolioItem({
       className="
     relative
     w-full
-    aspect-[4/3]          /* mobile เตี้ยลง */
-    sm:aspect-[4/3]
-    lg:aspect-[4/3]      /* desktop cinematic */
+    aspect-[4/3]
   "
     >
       <motion.div
         layoutId={project.id}
         onClick={onClick}
         whileHover={{ scale: 1.05 }}
+        whileTap={{ scale: 0.97 }}
         transition={{ type: "spring", stiffness: 300, damping: 25 }}
         className="
-      absolute inset-0
-      cursor-pointer
-      overflow-hidden
-      rounded-xl
-      bg-zinc-800
-    "
+    group
+    absolute inset-0
+    cursor-pointer
+    overflow-hidden
+    rounded-xl
+    bg-zinc-800
+  "
       >
         <Image
           src={project.image}
           alt={project.title}
           fill
-          sizes="
-        (max-width: 240px) 100vw,
-        (max-width: 1024px) 50vw,
-        25vw
-      "
           className="object-cover"
         />
+
+        {/* CLICK HINT (แสดงตลอด) */}
+        <div
+          className="
+      absolute inset-0
+      flex items-center justify-center
+      bg-black/20
+      opacity-100
+      transition
+      group-hover:bg-black/40
+    "
+        >
+          <div
+            className="
+             absolute 
+             bottom-2 right-2 
+
+        flex items-center gap-2
+        px-4 py-2
+        rounded-full
+        bg-black/60
+        backdrop-blur
+        text-white
+        text-sm
+        opacity-80
+        group-hover:opacity-100
+        group-hover:scale-105
+        transition
+      "
+          >
+            <AdsClick />
+            <span className="tracking-wide">Click here</span>
+          </div>
+        </div>
       </motion.div>
     </Box>
   );
